861819efca066d9fb890c25b93ac02083bcb92e3
Elvira Quiroz <elvira.quiroz.dp@gmail.com>
2020-05-16 16:18:02 +0000

    js listo, falta cambio de colores de pintas

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-let cardNumbers = [1,2,3,4,5,6,7,8,9,10,'J','Q','K']
-let suits = ['heart', 'diamond', 'spades', 'clubs']
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+let suit = ['&spades;', '&clubs;', '&hearts;', '&diams;']
+
+
+let suitRandom = suit[Math.floor(Math.random() * suit.length)]
+let numbersRandom = numbers[Math.floor(Math.random() * numbers.length)]
+
+
+function Onload() {
+    
+    document.getElementById("top-suit").innerHTML = suitRandom
+    document.getElementById("number").innerHTML = numbersRandom
+    document.getElementById("bottom-suit").innerHTML = suitRandom
+
+}
+
+ Onload() 
